06923cd5129160e54ad90d7ceea6758737591a00
Simone Schioppo <simoneschioppo@MacBook-Pro-di-Simone.local>
2023-08-30 00:01:01 +0200

    first commit for capstone project

---
+++ fe/fproject/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavbar from './components/Navbar';
+
+function App() {
+  return (
+      <MyNavbar />
+  );
+}
+
+export default App;
+
+
