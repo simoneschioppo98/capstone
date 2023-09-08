f6f94e9025dcb068d10ebccfdd2ffd50b86e0b51
Simone Schioppo <simoneschioppo@MacBook-Pro-di-Simone.local>
2023-09-09 00:44:07 +0200

    add api functionalities (in progress)

--- fe/fproject/src/components/flightContainer/flContainer.js
+++ fe/fproject/src/components/flightContainer/flContainer.js
@@ -1,34 +1,70 @@
 import React, { useState } from 'react';
 import './flight.css';
-import axios from 'axios';
+import FlightInfo from '../flightInfo.js/flightInfoContainer';
 
 function FlightSearch() {
-        const [airportName , setAirportName] = useState('')
-        const getFlightInfo = () => {
-          axios.get('http://api.aviationstack.com/v1/flights?access_key=061edb78b491ed015082d47701395cfe')
-          .then(res =>{
-            console.log(res.data)
-          }).catch(err => {
-            console.log('request error');
-          })
-        }
+  const [carrier, setCarrier] = useState('');
+  const [flightNumber, setFlightNumber] = useState('');
+  const [date, setDate] = useState('');
+  const [flightData, setFlightData] = useState(null);
+
+  const getFlightInfo = () => {
+    // Controlla che tutti i campi siano compilati
+    if (!carrier || !flightNumber || !date) {
+      alert('Compila tutti i campi prima di effettuare la ricerca.');
+      return;
+    }
+
+    let formattedDate = date.replace("-", "/");
+    formattedDate = formattedDate.replace("-", "/");
+    console.log(formattedDate);
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/${carrier}/${flightNumber}/arr/2023/09/09?appId=5a041ee5&appKey=642da6f80725f2a9b647e91775784030&utc=false`, {
+      method: "GET",
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Errore nella richiesta');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      setFlightData(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  };
 
   return (
     <div className="central">
       <div className="inner-container">
         <input
-        type="text"
-        placeholder='Airlines (e.g ITA AIRWAYS)'
+          type="text"
+          placeholder='Carrier (e.g AA)'
+          value={carrier}
+          onChange={e => setCarrier(e.target.value)}
         />
-           <input
-        type="text"
-        placeholder='Airport Name'
+        <input
+          type="text"
+          placeholder='Flight Number'
+          value={flightNumber}
+          onChange={e => setFlightNumber(e.target.value)}
         />
-           <input
-        type="date"
+        <input
+          type="text"
+          placeholder='Date (YYYY-MM-DD)'
+          value={date}
+          onChange={e => setDate(e.target.value)}
         />
       </div>
       <button onClick={getFlightInfo}>Search Flight</button>
+      {flightData && (
+        <div>
+          <FlightInfo/>
+        </div>
+      )}
     </div>
   );
 }
---
+++ fe/fproject/src/components/flightInfo.js/flightInfoContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './flightinfo.css';
+
+
+function FlightInfo({ data }) {
+  // Estrai le informazioni desiderate dai dati della chiamata API
+  const flightStatus = data?.flightStatuses?.[0]?.airline;
+  const departureAirport = data?.appendix?.airports?.[0]?.name;
+  const arrivalAirport = data?.appendix?.airports?.[1]?.name;
+
+  return (
+    <div className='oro'>
+      <h2>Flight Information</h2>
+      <p className='cazzo'>Flight Status: {flightStatus}</p>
+      <p>Departure Airport: {departureAirport}</p>
+      <p>Arrival Airport: {arrivalAirport}</p>
+    </div>
+  );
+}
+
+export default FlightInfo;
