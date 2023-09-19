5ec61e43f0e0965659111b4bc3d8401ad77bcd29
Simone Schioppo <simoneschioppo@MacBook-Pro-di-Simone.local>
2023-09-20 00:57:31 +0200

    add final meteo features

--- fe/fproject/src/components/WeatherContainer/meteoContainer.js
+++ fe/fproject/src/components/WeatherContainer/meteoContainer.js
@@ -1,11 +1,14 @@
+// MeteoContainer.js
 import React, { useState, useEffect } from "react";
 import './weatherContainer.css';
 import axios from "axios";
 import MeteoStatus from "../meteoResponse/meteoInfo";
+import {Switch} from 'antd';
 
 function MeteoContainer() {
   const [cityName, setCityName] = useState('');
   const [weatherData, setWeatherData] = useState(null);
+  const [temperatureScale, setTemperatureScale] = useState("Celsius");
 
   const getMeteoInfo = () => {
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=0a9433c5f8afc8a2e802c91678a55eb8`)
@@ -18,6 +21,14 @@ function MeteoContainer() {
       });
   }
 
+  const toggleTemperatureScale = () => {
+    if (temperatureScale === "Celsius") {
+      setTemperatureScale("Fahrenheit");
+    } else {
+      setTemperatureScale("Celsius");
+    }
+  }
+
   return (
     <div className="meteo-container">
       <div className="form">
@@ -27,16 +38,27 @@ function MeteoContainer() {
           value={cityName}
           onChange={e => setCityName(e.target.value)}
         />
-        <button onClick={getMeteoInfo}>Search Informations</button>
-        <button>C</button>
-        <button>F</button>
+        <button onClick={getMeteoInfo} className="info-button">Search Informations</button>
+        <Switch
+          checkedChildren="C"
+          unCheckedChildren="F"
+          checked={temperatureScale === "Celsius"}
+          onChange={toggleTemperatureScale}
+        />
       </div>
       <div className="meteo-status-container">
-        {weatherData && <MeteoStatus data={weatherData} />}
+        {weatherData && (
+          <MeteoStatus
+            data={weatherData}
+            temperatureScale={temperatureScale}
+          />
+        )}
       </div>
     </div>
   );
 }
 
 export default MeteoContainer;
+
+
 
--- fe/fproject/src/components/meteoResponse/meteoInfo.js
+++ fe/fproject/src/components/meteoResponse/meteoInfo.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import "./meteoResponse.css";
 import SkyClearDay from "../statimeteo/sole.jpg";
@@ -7,52 +8,85 @@ import SkyCloudyNight from "../statimeteo/nottenuvolosa.jpg";
 import RainyDay from "../statimeteo/pioggia.jpg";
 
 
+function MeteoStatus({ data, temperatureScale }) {
+  const name = data.name;
+  const countryLocation = data.sys.country;
+  // funzione per convertire i gradi da kelvin a celsius//
+  const tempKelvin = data.main.temp;
+  let realTemp = tempKelvin - 273.15;
+  let realFeelTemp = data.main.feels_like - 273.15;
+// conditional state per convertire poi i gradi celsius in farheneit
+  if (temperatureScale === "Fahrenheit") {
+    realTemp = (tempKelvin - 273.15) * 9/5 + 32;
+    realFeelTemp = (data.main.feels_like - 273.15) * 9/5 + 32;
+  }
 
-function MeteoStatus({ data }) {
-  const name = data?.name;
-  const countryLocation = data?.sys?.country;
-  const temp = data?.main?.temp;
-  const realTemp = Math.ceil(temp - 273.15);
-  const feelTemp = data?.main?.feels_like;
-  const realFeelTemp = Math.ceil(feelTemp - 273.15);
-  const icon = data?.weather[0]?.icon;
+  realTemp = Math.ceil(realTemp);
+  realFeelTemp = Math.ceil(realFeelTemp);
+  // variabili d'uso per stabilire il sorgere e calare del sole (valore dato dall'api key in millisecondi e trasformato in secodni moltiplicando per 1000)
+  const sunriseTime = data.sys.sunrise;
+  const sunriseSecondTime = new Date(sunriseTime * 1000);
+  const sunsetTime= data.sys.sunset;
+  const sunsetSecondTime = new Date(sunsetTime * 1000);
+  //variabile pressione
+  const initialPressure = data.main.pressure;
+  // variabile per le icone dinamiche
+  const icon = data.weather[0].icon;
   const iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
+  // variabile per definire umidità
+  const humidity = data.main.humidity;
+  // formattazione data odierna,utile anche per la formattazione del sorgere e calare del sole
   const oggi = new Date();
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const dataOdierna = oggi.toLocaleDateString('it-IT', options);
-  const tempMin = data?.main?.temp_min;
-  const realTempMin = Math.ceil((tempMin - 273.15));
-  const tempMax = data?.main?.temp_max;
-  const realTempMax = Math.ceil((tempMax - 273, 15));
+  const realSunriseTime = sunriseSecondTime.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });
+  const realSunseTime = sunsetSecondTime.toLocaleTimeString('it-IT', { hour: '2-digit', minute: '2-digit' });  
+  const tempMinKelvin = data.main.temp_min;
+  let realTempMin = tempMinKelvin - 273.15;
+  let tempMaxKelvin = data.main.temp_max;
+  let realTempMax = tempMaxKelvin - 273.15;
+// conditional per passaggio da celsius a farheneit per la temperatura percepita massima e minima
+  if (temperatureScale === "Fahrenheit") {
+    realTempMin = (tempMinKelvin - 273.15) * 9/5 + 32;
+    realTempMax = (tempMaxKelvin - 273.15) * 9/5 + 32;
+  }
 
-  const getBackgroundImage = () =>{
-    switch (icon){
+  realTempMin = Math.ceil(realTempMin);
+  realTempMax = Math.ceil(realTempMax);
+  //variabile per definire la visbilità
+  const visibility = data.visibility;
+  const realVisibility = visibility / 1000 ; // conversione centimetri in kilometri
+// switch case usato per inviare loghi dinamici in base allo stato del tempo(cartella statimeteo)
+  const getBackgroundImage = () => {
+    switch (icon) {
       case "01d":
         return `url(${SkyClearDay})`;
       case "01n":
-        return `url(${SkyClearNight})`
+        return `url(${SkyClearNight})`;
       case "02d":
         return `url(${SkyCloudyDay})`;
       case "02n":
         return `url(${SkyCloudyNight})`;
-        case "04d":
-          return `url(${SkyCloudyDay})`;
-        case "04n":
-          return `url(${SkyCloudyNight})`;
+      case "04d":
+        return `url(${SkyCloudyDay})`;
+      case "04n":
+        return `url(${SkyCloudyNight})`;
       case "10d":
-        return `url(${RainyDay})`;
       case "10n":
         return `url(${RainyDay})`;
+      default:
+        return "";
     }
   };
-
+// funzione per cambiare stile alle immagini di sfondo dinamiche
   const backGroundStyle = {
-    backgroundImage : getBackgroundImage(),
-    backgroundSize : "cover",
-    backgroundRepeat : "no-repeat",
+    backgroundImage: getBackgroundImage(),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
   };
 
   return (
+    <div className="section-container">
     <div className="main-section" style={backGroundStyle}>
       <div className="current-condition">
         <div className="header-condition">
@@ -62,14 +96,48 @@ function MeteoStatus({ data }) {
       </div>
       <div className="mid-condition" >
         <div className="weather-details">
-          <span>{realTemp}°C</span>
-          <p>temperatura percepita:{realFeelTemp}°C</p>
-          <p>MIN:{realTempMin}°/MAX:{realTempMax}°</p>
+          <span>{realTemp}°{temperatureScale === "Fahrenheit" ? "F" : "C"}</span>
+          <p>temperatura percepita: {realFeelTemp}°{temperatureScale === "Fahrenheit" ? "F" : "C"}</p>
+          <p>MIN: {realTempMin}°{temperatureScale === "Fahrenheit" ? "F" : "C"}/MAX: {realTempMax}°{temperatureScale === "Fahrenheit" ? "F" : "C"}</p>
         </div>
         <div className="icon-condition">
           <img src={iconUrl} alt="immagine dinamica meteo" />
         </div>
       </div>
+    </div>
+    <div className="lower-section">
+      <div className="today-details">
+          <div className="today-city-informations">
+              <h2>il meteo di oggi a {name}</h2>
+          </div>
+          <div className="mid-information">
+            <div className="sun-info">
+            <span class="material-symbols-outlined">water_lux</span>
+             <span>{realSunriseTime}</span>
+             <span class="material-symbols-outlined">wb_twilight</span>
+             <span>{realSunseTime}</span>
+            </div>
+          </div>
+          <div className="last-details">
+              <div className="inner-details">
+              <span class="material-symbols-outlined">compress</span>
+                <p>pressione atmosferica</p>
+                <span>{initialPressure} Pa</span>
+              </div>
+              <div className="inner-details">
+              <span class="material-symbols-outlined">humidity_percentage</span>
+                <p>pressione atmosferica</p>
+                <span>{humidity} %</span>
+              </div>
+              <div className="inner-details">
+              <span class="material-symbols-outlined">visibility</span>
+                <p>pressione atmosferica</p>
+                <span>{realVisibility}KM</span>
+              </div>
+          </div>
+      </div>
+
+    </div>
     </div>
   );
 }
